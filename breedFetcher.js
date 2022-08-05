b1abe674dd894d8c1c850d9771590a378893aeeb
DohBae <bdoherty92@msn.com>
2022-08-05 00:45:17 +0000

    Fixed some linting errors and added mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,24 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log('Error: ', error);
-  } else if (data.length === 0)  {
-    console.log("There has been an error. Please try again.");
-  } else {
-    let description = data[0].description;
-    console.log(description);
-  }
-});
+const fetchBreedDescription = function(breed, myCBFxn) {
 
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      myCBFxn(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0)  {
+      myCBFxn("Breed not found.", null);
+    } else {
+      let description = data[0].description;
+      myCBFxn(null, description);
+    }
+  });
+};
 
-
-
-
-
-
+module.exports = {fetchBreedDescription};
 
 
 
-/*
-A little more scaffolding would have been helpful while working on the assignment. I really struggled with the last part and trying to have a user type in just the cat name to find a breed description.
 
-*/
